84793541a856425efdf5965830fa3604c7459d33
Vadim Samarin <dezivert@gmail.com>
2021-02-20 19:32:19 +0200

    cleanup

--- src/hmr/index.ts
+++ src/hmr/index.ts
@@ -8,12 +8,13 @@ export enum EDirTypeContent {
 }
 
 /**
+ * DOES NOT WORK. USE COMMENTS INSTEAD
  * Installs Webpack handlers to load contents of path
  * Requires Plugin to be enabled
  * @param engine
  * @param type
  * @param path
  */
-export function setupContentPath(engine: Engine, type: EDirTypeContent, path: string) {
+export function setupContentPath(type: EDirTypeContent, path: string, engine: Engine) {
   throw new Error('Eacy plugin is not enabled.');
 }
