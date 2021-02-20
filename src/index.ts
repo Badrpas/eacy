d35d8c66a06cff1c94750038af16c9cc67267e16
Vadim Samarin <dezivert@gmail.com>
2021-02-20 19:54:11 +0200

    Comment eacy-ui import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export * as HMR from './hmr';
 export { IGNORE_MARK, IMPORTED_FROM } from './hmr'
 
-// @ts-ignore
-export { setupUI } from 'eacy-ui';
 export * from 'eaciest';
 
 import './warn-saver';
